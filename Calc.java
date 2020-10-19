53f2bd58ca9f87fdf0d9343d99425cf108fdd6fe
gainesarit <65114492+gainesarit@users.noreply.github.com>
2020-10-19 18:04:28 +0530

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -1,8 +1,8 @@
 class Calc{
   public static int add(int a,int b){
-   return a+b;
+   return (a+b);
   }
  public static void main(String[] args){
-  add(10,20);
+  System.out.println(add(10,20));
  }
 }
